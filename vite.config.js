be3933416e0eac00309cd0747cc5db6d318cb6a6
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-12-10 09:18:33 +0600

    set repo

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { ghPages } from 'vite-plugin-gh-pages'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),ghPages()],
-  base:'https://github.com/Shondarzer-Taroka/Ass2'
+  base:'Ass2'
 })
