da440f248a228581ec4f38e841127c84cdbbc58f
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-12-10 09:13:32 +0600

    file store

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'https://shondarzer-taroka.github.io/Ass2'
+  plugins: [react(),ghPages()],
+  base:'https://github.com/Shondarzer-Taroka/Ass2'
 })
